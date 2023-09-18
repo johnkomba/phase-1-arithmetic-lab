87499496a03dcb7728853c002913713a90678b19
Jkomba <johnkombah88@gmail.com>
2023-09-18 17:26:28 +0300

    done w assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+// Define the variables and functions
+const num1 = 31;
+const num2 = 2;
+const num3 = 40;
+const num4 = 9;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20);
+
+module.exports = { multiply, random, mod, max };
